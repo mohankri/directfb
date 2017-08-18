0237a509dcf81c51c838dcf287c6122e6db66bf1
Krishna Mohan <mohankri@gmail.com>
2017-08-18 14:57:15 -0700

    update dfb

--- dfb.c
+++ dfb.c
@@ -22,25 +22,50 @@ int main (int argc, char **argv)
 {
   DFBSurfaceDescription dsc;
 
+  /* Initialize Direct FB */
   DFBCHECK (DirectFBInit (&argc, &argv));
-  DFBCHECK (DirectFBCreate (&dfb));
-  DFBCHECK (dfb->SetCooperativeLevel (dfb, DFSCL_FULLSCREEN));  
 
+  /* Create Super Interface */
+  DFBCHECK (DirectFBCreate (&dfb));
+
+  /* Go Full Screen */
+  DFBCHECK (dfb->SetCooperativeLevel (dfb, DFSCL_FULLSCREEN));  
+  //DFBCHECK(dfb->SetCooperativeLevel(dfb, DFSCL_NORMAL));
+
+  /* Flippable Primary Surface */
   dsc.flags = DSDESC_CAPS;
   dsc.caps  = DFBSurfaceCapabilities(DSCAPS_PRIMARY | DSCAPS_FLIPPING);
 
+  /* Create Surface */
   DFBCHECK (dfb->CreateSurface( dfb, &dsc, &primary ));
+
+  /* Get Screen Resolution of Primary Surface */
   DFBCHECK (primary->GetSize (primary, &screen_width, &screen_height));
-  DFBCHECK (primary->FillRectangle (primary, 0, 0, screen_width, screen_height));
+
+  DFBCHECK (primary->FillRectangle (primary, 0, 0, screen_width/2, screen_height/2));
 
   DFBCHECK (primary->SetColor (primary, 0x80, 0x80, 0xff, 0xff));
   DFBCHECK (primary->DrawLine (primary,
 			                      0, screen_height / 2,
 			       screen_width - 1, screen_height / 2));
   DFBCHECK (primary->Flip (primary, NULL, DFBSurfaceFlipFlags(0)));
+/*
+  for (;;) {
+    char text[4096];
+    fgets(text, sizeof(text), stdin);
+    strtok(text, "\n");
 
+    if (!strcmp(text, "QUIT")) 
+      break;
+    else if (!strcmp(text, "FLIP")) {
+      primary->SetColor(primary, 0x20, 0x20, 0xff, 0xff);
+    } else {
+      primary->SetColor(primary, 0x40, 0x40, 0xff, 0xff);
+    }
+  } */
   sleep (5);
 
+  /* cleanup primary and super interface */
   primary->Release( primary );
   dfb->Release( dfb );
 
